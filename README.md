00a3f3b0ab02f733f646e00a3a234360c83229b5
Vitor Cruz <vitorengcruz@gmail.com>
2019-08-30 19:42:37 -0300

    CE-235 ExLab2 - Resolucao do exercicio laboratorio 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CE235-ExLab2
+Repositório para armazenamento do ExLab2 do curso de pós-graduação ITA CE-235
---
+++ exercicioLab2/Sources/os_tasks.c
@@ -0,0 +1,127 @@
+/* ###################################################################
+**     Filename    : os_tasks.c
+**     Project     : exercicioLab2
+**     Processor   : MKL25Z128VLK4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2019-08-29, 16:47, # CodeGen: 1
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Task2_task - void Task2_task(os_task_param_t task_init_data);
+**         Task1_task - void Task1_task(os_task_param_t task_init_data);
+**
+** ###################################################################*/
+/*!
+** @file os_tasks.c
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup os_tasks_module os_tasks module documentation
+**  @{
+*/         
+/* MODULE os_tasks */
+
+#include "Cpu.h"
+#include "Events.h"
+#include "rtos_main_task.h"
+#include "os_tasks.h"
+
+#include "ledrgb_hal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Callback    : Task2_task
+**     Description : Task function entry.
+**     Parameters  :
+**       task_init_data - OS task parameter
+**     Returns : Nothing
+** ===================================================================
+*/
+void Task2_task(os_task_param_t task_init_data)
+{
+  /* Write your local variable definition here */
+  
+#ifdef PEX_USE_RTOS
+  while (1) {
+#endif
+    /* Write your code here ... */
+    
+	ledrgb_clearRedLed();
+    
+	OSA_TimeDelay(1000);
+   
+	ledrgb_setRedLed();
+
+	OSA_TimeDelay(1000);
+    
+    
+#ifdef PEX_USE_RTOS   
+  }
+#endif    
+}
+
+/*
+** ===================================================================
+**     Callback    : Task1_task
+**     Description : Task function entry.
+**     Parameters  :
+**       task_init_data - OS task parameter
+**     Returns : Nothing
+** ===================================================================
+*/
+void Task1_task(os_task_param_t task_init_data)
+{
+  /* Write your local variable definition here */
+  
+#ifdef PEX_USE_RTOS
+  while (1) {
+#endif
+    /* Write your code here ... */
+    
+    
+	ledrgb_setGreenLed();
+
+	OSA_TimeDelay(1000);
+   
+	ledrgb_clearGreenLed();
+
+	OSA_TimeDelay(1000);
+
+    
+    
+#ifdef PEX_USE_RTOS   
+  }
+#endif    
+}
+
+/* END os_tasks */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
